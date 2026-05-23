92e9807696343c090d3b0b21bc874b10ec2d58ed
Michael Kaiser <kaiser_michael@gmx.at>
2026-05-23 07:28:40 +0200

    Update

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+# It allows you to define things like:
+#
+# reusable test data
+# fixtures (setup/teardown logic)
+# database connections
+# API clients
+# test configuration
+# hooks for customizing pytest behavior
+# Is valid here and for sub folders
